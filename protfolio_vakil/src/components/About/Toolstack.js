b92c8ae2594158bd0a2cd8b31199616ddaeb15e3
vakilKumar <vakilraj26031997@gmail.com>
2024-02-18 20:04:50 +0530

    first commit

---
+++ protfolio_vakil/src/components/About/Toolstack.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMacos,
+  SiXcode,
+  SiAndroidstudio,
+  SiJirasoftware,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiXcode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAndroidstudio />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJirasoftware />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMacos />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
