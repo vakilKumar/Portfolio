375eee550bfc665361b345f6a55392eb108aab50
vakilKumar <vakilraj26031997@gmail.com>
2024-02-18 21:03:43 +0530

    Revert "Merge branch 'main' of https://github.com/vakilKumar/portfolio"
    This reverts commit 88c55bee0fe737c4e9cd07d5533414f7729fc5db, reversing
    changes made to d628cb78b107f65d9f4190027a6da41ec645ce30.

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Vakil Kumar </span>
+            from <span className="purple"> Sitamarhi, Bihar, India.</span>
+            <br />
+            I am currently employed as a software developer at Uneecops Technologies.
+            <br />
+            I have completed B.E in Electrical at Lakshmi Narain College of Technology, indore.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listening Music
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Vakil</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
